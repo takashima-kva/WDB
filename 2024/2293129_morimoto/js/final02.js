f3af6eab92ea15c42aed69a9f3ad942375737b53
takashima-kva <103571599+takashima-kva@users.noreply.github.com>
2024-07-18 09:53:24 +0900

    Add files for WDB2024 ver01

---
+++ 2024/2293129_morimoto/js/final02.js
@@ -0,0 +1,20 @@
+$(".gallery-list").modaal({
+	fullscreen:'true', //フルスクリーンモードにする
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
+
+$(".info").modaal({
+	overlay_close:true,//モーダル背景クリック時に閉じるか
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
+
